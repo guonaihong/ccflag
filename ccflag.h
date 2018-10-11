f845acf0fd04ce7f450651fb7717573e6703edde
guonaihong <guonaihong@qq.com>
2018-10-11 22:18:07 +0800

    更新代码

--- ccflag.h
+++ ccflag.h
@@ -1,8 +1,17 @@
 #ifndef __CCFLAG_H
 
+#include <functional>
+#include <map>
 #include <string>
 
 using string = std::string;
+
+class CCFlag{
+    void  *val;
+    string name;
+    string defvalue;
+    string usage;
+};
 
 class CCFlagSet {
     string name;
@@ -15,14 +24,23 @@ class CCFlagSet {
 
         void string_var(string *p, string name, string value, string usage);
     private:
-        void var(string name, string value, string usage);
+        std::map<string, CCFlag> formal;
+
+        void var(string name, string value, string usage, std::function<void (string, string, string)> func);
 };
 
 
+void CCFlagSet::var(string name, string value, string usage, std::function<void (string, string, string)> func) {
+    func(name, value, usage);
+}
+
 void CCFlagSet::bool_var(bool *p, string name, string value, string usage) {
-    [this]{
+    /*
+    auto f = [this]{
         printf("%s\n", this->name.c_str());
-    }();
+    };
+    var(name, value, usage, f);
+    */
 }
 
 void CCFlagSet::int_var(int *p, string name, string value, string usage) {
