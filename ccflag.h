b92a8126f83d6719ffc66569f1353f0234def9f0
guonaihong <guonaihong@qq.com>
2018-10-12 22:05:08 +0800

    完善下bool策略

--- ccflag.h
+++ ccflag.h
@@ -3,52 +3,98 @@
 #include <functional>
 #include <map>
 #include <string>
+#include <iostream>
+#include <stdlib.h>
 
 using string = std::string;
 
 class CCFlag{
+    public:
     void  *val;
     string name;
     string defvalue;
     string usage;
+
+    std::function<void (string value)> func;
 };
 
 class CCFlagSet {
+
     string name;
+    std::map<string, CCFlag> formal;
+
     public:
-        void bool_var(bool *p, string name, string value, string usage);
+        void bool_var(bool *p, string name, string defvalue, string usage);
 
-        void int_var(int *p, string name, string value, string usage);
+        void int_var(int *p, string name, string defvalue, string usage);
 
-        void double_var(double *p, string name, string value, string usage);
+        void double_var(double *p, string name, string defvalue, string usage);
 
-        void string_var(string *p, string name, string value, string usage);
+        void string_var(string *p, string name, string defvalue, string usage);
     private:
-        std::map<string, CCFlag> formal;
 
-        void var(string name, string value, string usage, std::function<void (string, string, string)> func);
+        void var(void *p, string name, string value, string usage, std::function<bool (string value)> func);
 };
 
 
-void CCFlagSet::var(string name, string value, string usage, std::function<void (string, string, string)> func) {
-    func(name, value, usage);
+void CCFlagSet::var(void *p, string name, string defvalue, string usage, std::function<bool (string value)> func) {
+    CCFlag flag = {p, name, defvalue, usage, func};
+
+    auto v = formal.count(name);
+    if (v > 0) {
+        string msg = "";
+        if (this->name.length() > 0) {
+            msg = "ccflag redefined:" + name;
+        } else {
+            msg = this->name + "ccflag redefined:" + name;
+        }
+
+        std::cout << msg << std::endl;
+        exit(1);
+    }
+
+    formal[name] = flag;
 }
 
-void CCFlagSet::bool_var(bool *p, string name, string value, string usage) {
-    /*
-    auto f = [this]{
-        printf("%s\n", this->name.c_str());
+void CCFlagSet::bool_var(bool *p, string name, string defvalue, string usage) {
+    auto f = [=](string value)->bool{
+        auto parse_bool = [](string str) -> int{
+            if (str == "1" || str == "t" || str == "T") {
+                return 1;
+            }
+
+            if (str == "true" || str == "TRUE"||str == "True") {
+                return 1;
+            }
+
+            if (str == "0" || str == "f" || str == "F") {
+                return 0;
+            }
+
+            if (str=="false" || str == "FALSE" ||str == "False") {
+                return 0;
+            }
+
+            return -1;
+        };
+
+        auto b = parse_bool(value);
+        if (b == -1) {
+            return false;
+        }
+
+        *p = (b == 1) ? true : false;
     };
-    var(name, value, usage, f);
-    */
+
+    var(p, name, defvalue, usage, f);
 }
 
-void CCFlagSet::int_var(int *p, string name, string value, string usage) {
+void CCFlagSet::int_var(int *p, string name, string defvalue, string usage) {
 }
 
-void CCFlagSet::double_var(double *p, string name, string value, string usage) {
+void CCFlagSet::double_var(double *p, string name, string defvalue, string usage) {
 }
 
-void CCFlagSet::string_var(string *p, string name, string value, string usage) {
+void CCFlagSet::string_var(string *p, string name, string defvalue, string usage) {
 }
 #endif
